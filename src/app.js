c8feed9212651d6c4678957196927a42403b747c
fabbioanderson@gmail.com <fabbioanderson@gmail.com>
2021-02-08 20:38:56 -0300

    aula 8 fevereiro

--- src/app.js
+++ src/app.js
@@ -1,25 +1,28 @@
-//CRIAR A ESTRUTURA DA APLICAÇÂO
+// CRIAR A ESTRUTURA DA APLICAÇÂO
 
- import express from 'express'
- import routes from './routes'
+import express from 'express';
+import routes from './routes';
 
- class App {
-   constructor() {
-     this.server = express(); // igual a variavel roras
-     this.middlewares();
-     this.routes();
-   }
-   middlewares() {
-     //cadastrar todos os middlewares da aplicação
+import './database';
 
-     this.server.use(express.json());
-   }
- 
-   routes() {
-      this.server.use(routes);
-   }
- }
- 
- //module.exports = new App().server; //exportar o server para outro arquivo;
- 
- export default new App().server;
+class App {
+  constructor() {
+    this.server = express(); // igual a variavel roras
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // cadastrar todos os middlewares da aplicação
+
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+// module.exports = new App().server; //exportar o server para outro arquivo;
+
+export default new App().server;
